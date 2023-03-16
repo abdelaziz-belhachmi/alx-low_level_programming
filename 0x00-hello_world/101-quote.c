fb831f3c44a9c1971477968bcd6f3e40259af245
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 05:50:59 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main ()
+{
+char c[100] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-int main (){
+int i =0;
 
-	char c[100] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	int i =0;
-	while (c[i]!='\0'){
-	putc(c[i],stdout);
-	i++;
-	}
-	
-	return 1;
+while (c[i]!='\0'){
+putc(c[i],stdout);
+i++;
+}
+return 1;
 }
