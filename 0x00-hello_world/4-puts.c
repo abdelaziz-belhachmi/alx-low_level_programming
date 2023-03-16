0a27adde0f016abd4651933699b3e6bb4071d060
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 04:25:37 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *     * multilingual puzzle" to the standard output using the `puts` function.
  *      *
  *       * Return: Always 0 (Success)
- *        */
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
