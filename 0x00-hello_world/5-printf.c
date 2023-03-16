db3ec7311ea8228a0927d5eb10947ffecf73ce7d
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 04:43:54 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * print "with proper grammar, but the outcome is a piece of art,"
+ *
+ *  print "with proper grammar, but the outcome is a piece of art,"
+ * 
  *  to the standard output using the printf function.
- *  Return: Always 0 (Success)
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,");
-
-	        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
