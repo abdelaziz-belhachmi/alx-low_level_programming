d11439bb585f7687567ec8c8696aa9f55a822d63
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 04:37:51 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/*
+ *  * main - Entry point
+ *   *
+ *    * Description: This function prints the string "with proper grammar, but the outcome is a piece of art,"
+ *     *              to the standard output using the puts function.
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
+int main(void)
+{
+	    printf("with proper grammar, but the outcome is a piece of art,");
+
+	        return (0);
+}
