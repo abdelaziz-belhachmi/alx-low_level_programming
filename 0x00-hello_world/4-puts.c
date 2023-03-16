aa3078acaa7d79c4b7c4d12f25a7b9eaa700a042
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 04:17:21 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){
-puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+puts("\n");
+return 0;
 }
