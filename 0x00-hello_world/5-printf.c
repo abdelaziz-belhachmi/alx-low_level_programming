ca2c9c8b914c8fe95e24a3526946cac7d464c5c8
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 04:47:51 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - Entry point
  *
  *  print "with proper grammar, but the outcome is a piece of art,"
- * 
+ *
  *  to the standard output using the printf function
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
