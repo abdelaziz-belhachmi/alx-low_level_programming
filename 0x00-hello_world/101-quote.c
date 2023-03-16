45baadf5501ff0cb863945e13faf64942fc12bb3
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-16 05:54:03 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,16 @@
  *   *
  *    * Return: Always 0 (Success)
  */
-int main ()
+int main(void)
 {
-char c[100] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char c[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-int i =0;
+int i = 0;
 
-while (c[i]!='\0'){
-putc(c[i],stdout);
+while(c[i]! = '\0') 
+{
+putc(c[i], stdout);
 i++;
 }
-return 1;
+return (1);
 }
