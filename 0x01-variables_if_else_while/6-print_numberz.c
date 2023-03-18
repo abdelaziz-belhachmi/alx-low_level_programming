b4c659552ce5674f82abd49b55fad4ee71938eb7
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-17 17:07:00 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- main
+ *
+ * Return:always 0 (Success)
+ */
+int main(void)
+{
+int A[10] = {0,1,2,3,4,5,6,7,8,9};
+int i = 0;
+while (i < 10)
+{
+putchar(A[i]+'0');
+i++;
+}
+	return (0);
+}
