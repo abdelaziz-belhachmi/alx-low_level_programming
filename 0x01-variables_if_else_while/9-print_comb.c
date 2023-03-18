57998c2f94ff45a07eca27578fe066896d5c9864
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-17 17:36:17 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main- main
+ * main -main
  *
- * Return:always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
-int A[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-int i = 0;
-while (i < 9)
+int i;
+for (i = 0; i < 10 ; i++)
 {
-putchar(A[i] + ',');
-putchar(' ');
-i++;
+putchar (i + '0');
+if (i != 9)
+{
+putchar(',');
 }
-putchar('9');
-putchar('\n');
-	return (0);
+}
+putchar ('\n');
+return (0);
 }
