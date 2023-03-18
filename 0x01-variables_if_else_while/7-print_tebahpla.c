918d7a09efe41d965db6b2a7e61deafc472221f8
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-17 17:17:52 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- main
+ *
+ * Return:always 0 (Success)
+ */
+int main(void)
+{
+char Alphabets[] = "zyxwvutsrqponmlkjihgfedcba\n";
+int i = 0;
+
+while (Alphabets[i] != '\0')
+{
+putchar(Alphabets[i]);
+i++;
+}
+	return (0);
+}
