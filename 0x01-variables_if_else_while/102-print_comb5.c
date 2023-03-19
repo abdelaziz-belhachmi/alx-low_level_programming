05e10cecd16418f742b2ce06fee35636a8867168
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-19 11:03:50 -0700

    advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 /**
- * main- main
- *
- * Return:always 0 (Success)
- */
+* main- main
+*
+* Return:always 0 (Success)
+*/
 int main(void){
-int i, j;
 int a, b;
-for (a = 0; a < 10; a++)
+for (a = 0; a < 100; a++)
 {
-for (b = 0; b < 10; b++)
-{
-for (i = a; i < 10; i++)
-{
-for (j = b + 1; j < 10; j++)
+for (b = a+1; b < 100; b++)
 {
 putchar(a + '0');
-putchar(b + '0');
 putchar(' ');
-putchar(i + '0');
-putchar(j + '0');
-if (b < 10)
+putchar(b + '0');
+if (a < 99)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
