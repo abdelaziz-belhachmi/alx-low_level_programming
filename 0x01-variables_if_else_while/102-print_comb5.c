61e18232f1663c4552b1e297a08d0c3fe0d5b267
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-19 14:41:33 -0700

    advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,14 +9,14 @@ int main(void)
 int a, b;
 for (a = 0; a < 100; a++)
 {
-for (b = a+1; b < 100; b++)
+for (b = a + 1; b < 100; b++)
 {
 putchar(a / 10 + '0');
 putchar(a % 10 + '0');
 putchar(' ');
 putchar(b / 10 + '0');
 putchar(b % 10 + '0');
-if (a < 99)
+if (a < 98)
 {
 putchar(',');
 putchar(' ');
