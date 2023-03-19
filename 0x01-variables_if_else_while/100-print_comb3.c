ca6ce5e0e604a19e9f41181bf25d3e186ffd15e0
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-19 09:01:19 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 /**
- * @brief 
+ * main - main 
  * 
  * @return 0 
  */
 int main (void){
 int i;
 int j;
-for(i=0;i<10;i++){
-for(j=i+1;j<10;j++){
-putchar(i+'0');
-putchar(j+'0');
-if (i<8)
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
 {
 putchar(',');
 putchar(' ');
 }
 }    
 }
+putchar('\n');
 return (0);
 }
