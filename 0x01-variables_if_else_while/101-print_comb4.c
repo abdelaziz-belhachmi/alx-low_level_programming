2a0baef11435824f6b66b745a65c9143689e1846
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-19 09:29:23 -0700

    advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- main
+ *
+ * Return:always 0 (Success)
+ */
+int main(void){
+int i,j,t;
+for ( i = 0; i < 10; i++){
+for( j = i + 1; j < 10; j++){
+for ( t = j + 1; t < 10; t++){
+putchar(i+'0');
+putchar(j+'0');
+putchar(t+'0');
+if (i<7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+return (0);
+}
