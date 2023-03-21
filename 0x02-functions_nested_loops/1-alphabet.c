12f30ac835437ca5cd346cca66a69d93763b6981
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-21 15:49:07 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - main 
+ *
+ *return:always 0 (success)
+ */
+void print_alphabet(void)
+{
+char alpha = "abcdefghijklmnopqrstuvwxyz\n";
+int i = 0;
+while ( alpha[i] != '\0' )
+{
+putchar((int)alpha[i]);
+i++;
+}
+return (0);
+}
