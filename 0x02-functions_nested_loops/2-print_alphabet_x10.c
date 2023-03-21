377abb31cdf7022c7f78e4d8e5201cf5132c2d6e
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-21 16:50:52 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void);
 void print_alphabet_x10(void)
 {
 char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
-int i = 0;, 
+int i = 0;
 int a = 0;
-while(a < 10)
+while (a < 10)
 {
 while (alpha[i] != '\0')
 {
