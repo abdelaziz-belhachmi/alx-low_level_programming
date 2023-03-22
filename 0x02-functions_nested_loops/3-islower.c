a171f00ac5e5a3ba3885b501fc521965c9629cb0
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-21 17:12:29 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _islower -
+ *
+ * Return:always (Success)
+ */
+int _islower(int c);
+int _islower(int c)
+{
+if (c > 97 && c < 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
