442c7d86b84b021954aa8a8ff69cb554461ee4a5
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-22 18:34:10 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *_isalpha -
- * @c i a variable that we need to check if its a char
- * Return : either 1 or 0 (Success)
- * */
+ *_isalpha - function to check is a char is an alphabet
+ * @c: is a variable that we need to check if it is a char
+ * Description: if alphabet return 1 else return 0
+ * Return: either 1 or 0 (Success)
+ */
 int _isalpha(int c);
 int _isalpha(int c)
 {
