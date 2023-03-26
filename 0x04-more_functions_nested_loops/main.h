69a83088cc5ff0d8ca080de3956b890d0f88cb84
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-26 09:35:04 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet();
+void print_alphabet_x10(void);
+int _islower(int c);
+int _putchar(char c);
+int _isalpha(int c);
+int _isupper(int c);
+#endif 
