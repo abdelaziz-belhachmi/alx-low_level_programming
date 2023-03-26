2330a1fcae896bb0dbee66afb88453d7a0e567e2
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-26 10:10:00 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+for (int a = 47; a < 58; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
