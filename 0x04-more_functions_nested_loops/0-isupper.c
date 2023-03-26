b0cceaebe159a70af0f0342162a03f1b4fbe966c
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-26 09:42:52 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *_isupper:_isuppper
- *@c:ascii of any char given
+ *_isupper - _isuppper
+ *@c: ascii of any char given
  *Return: 1 pr 0 (Success)
  */
 int _isupper(int c)
