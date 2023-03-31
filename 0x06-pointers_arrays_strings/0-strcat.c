8ab4848a64303d32b9f4736f3644f41700ed8351
abdelaziz-belhachmi <saha26.398@gmail.com>
2023-03-31 16:28:48 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,15 @@
 #include <string.h>
 /**
  * _strcat - cat
- *@*dest: char
- *@*src: char
+ *@dest: char
+ *@src: char
  *Return: a
  */
 char *_strcat(char *dest, char *src)
 {
 char *a = dest;
 char *b = src;
-strcat(a,b);
+
+strcat(a, b);
 return (a);
 }
